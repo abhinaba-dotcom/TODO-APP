d1b5b1172bfa4184ac759b8d1c76542c2242e3d1
Abhinaba Jash <abhinaba@itobuz.com>
2025-09-23 15:41:05 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,128 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
+  
+  function markTodo(currentElement, todoId) {
+    if (localStorage.getItem("todos")) {
+      const storedTodos = JSON.parse(localStorage.getItem("todos"));
+      if (currentElement.checked) {
+        //The code for to mark check is here
+        storedTodos.forEach((todo, index, array) => {
+          if (todo.id == todoId) {
+            const checkedTodo = { ...todo, isChecked: true };
+            array[index] = checkedTodo;
+          }
+        });
+      } else {
+        storedTodos.forEach((todo, index, array) => {
+          if (todo.id == todoId) {
+            const checkedTodo = { ...todo, isChecked: false };
+            array[index] = checkedTodo;
+          }
+        });
+      }
+      localStorage.setItem("todos", JSON.stringify(storedTodos));
+      setTimeout(() => {
+        fetchAllTodos();
+      }, 1000);
+    }
+  }
+  function editTodo(currentElement, todoId) {
+    if (localStorage.getItem("todos")) {
+      const storedTodos = JSON.parse(localStorage.getItem("todos"));
+      const todoTextPara =
+        currentElement.parentNode.previousElementSibling.lastElementChild;
+      todoTextPara.contentEditable = true;
+      todoTextPara.style.outline = "none";
+      todoTextPara.style.outline = "none";
+      todoTextPara.focus();
+      currentElement.style.display = "none";
+      currentElement.previousElementSibling.style.display = "flex";
+      currentElement.previousElementSibling.addEventListener("click", (e) => {
+        let editedTodoText = todoTextPara.innerText;
+        currentElement.style.display = "flex";
+        currentElement.previousElementSibling.style.display = "none";
+        storedTodos.forEach((todo, index, array) => {
+          if (todo.id == todoId) {
+            const editedTodo = {
+              todoText: editedTodoText,
+              timeStamp: todo.id,
+              isChecked: false,
+            };
+            array[index] = editedTodo;
+          }
+        });
+        localStorage.setItem("todos", JSON.stringify(storedTodos));
+        setTimeout(() => {
+          fetchAllTodos();
+        }, 1000);
+      });
+    }
+  }
+  function deleteTodo(todoId) {
+    if (localStorage.getItem("todos")) {
+      const storedTodos = JSON.parse(localStorage.getItem("todos"));
+      const filteredTodosAfterDeletion = storedTodos.filter(
+        (item) => item.id != todoId
+      );
+      localStorage.setItem("todos", JSON.stringify(filteredTodosAfterDeletion));
+      fetchAllTodos();
+    }
+  }
+  window.myGlobalEditTodo = editTodo;
+  window.myGlobalDeleteTodo = deleteTodo;
+  window.myGlobalMarkTodo = markTodo;
+  fetchAllTodos();
+  function fetchAllTodos() {
+    let allTodo = [];
+    if (!localStorage.getItem("todos")) {
+      return;
+    }
+    allTodo = JSON.parse(localStorage.getItem("todos"));
+    const todoContainer = document.getElementById("todo-container");
+    todoContainer.innerHTML = "";
+    if (allTodo) {
+      allTodo.forEach((todo) => {
+        let newTodoDiv = document.createElement("div");
+        newTodoDiv.innerHTML = `<div id=${todo.id} class="todo-item">
+            <div class="todo-item__todo-box">
+              <input class="check-button" onchange="myGlobalMarkTodo(this, ${
+                todo.id
+              })"  type="checkbox" ${todo.isChecked ? "checked" : ""} />
+              <div class="parent">
+              <p class="todo-text">${todo.todoText}</p>
+              <div class="strike-through-line"></div>
+              </div>
+            </div>
+            <div class="todo-container__button-box">
+              <img class="saveBtn" width="25px" height="25px" src="./src/assets/save.svg" alt="">
+              <img class="editBtn" onclick="myGlobalEditTodo(this,'${
+                todo.id
+              }')" width="25px" height="25px" src="./src/assets/edit.svg" alt="">
+              <img class="deleteBtn"  onclick="myGlobalDeleteTodo('${
+                todo.id
+              }')" width="25px" height="25px" src="./src/assets/delete.svg" alt="">
+            </div>
+          </div>`;
+        todoContainer.append(newTodoDiv);
+      });
+    }
+  }
+
+  function addNewTodo(todo) {
+    if (!localStorage.getItem("todos")) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    }
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    storedTodos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(storedTodos));
+    fetchAllTodos();
+  }
+
+  document.getElementById("saveBtn").addEventListener("click", (e) => {
+    e.preventDefault();
+    const todoText = document.getElementById("todo-input").value;
+    const timeStamp = Date.now();
+    const newTodo = { todoText: todoText, id: timeStamp, isChecked: false };
+    addNewTodo(newTodo);
+  });
+});
